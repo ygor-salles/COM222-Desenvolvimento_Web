911a3c8267860b25735314b7d83956886485c82a
unknown <ygorsallesaniceto@gmail.com>
2020-09-11 21:53:00 -0300

    terminando trabalho 3

--- TRABALHOS/02 - DOM/Exercicio3.js
+++ TRABALHOS/02 - DOM/Exercicio3.js
@@ -1,17 +1,25 @@
 function zoomOut() {
-    var imagens = [];
-    var classeAtual = '';
-    var tamAtual = 0;
+    const arrayFoto = document.querySelectorAll('[foto]')
+    const foto = document.querySelector('[foto]') 
+    if (foto.className == '')
+        arrayFoto.forEach(i => {
+        i.classList.add('img150')
+    })
+    else if (foto.className == 'img50')
+        arrayFoto.forEach(i => {
+            i.classList.remove('img50')
+        })
+}
 
-    imagens = document.getElementsByClassName('selecao')
-
-    classeAtual = (imagens[0].className)
-    tamAtual = parseInt(classeAtual.replace('selecao', ''))
-    console.log('tamAtual :', tamAtual)
-
-    if (tamAtual != 1) {
-        tamAtual--
-        for (let i=0; i<imagens.length; i++) 
-            imagens[i].className = 'selecao' + tamAtual
-    }
+function zoomIn() {
+    const arrayFoto = document.querySelectorAll('[foto]')
+    const foto = document.querySelector('[foto]') 
+    if (foto.className == '')
+        arrayFoto.forEach(i => {
+        i.classList.add('img50')
+    })
+    else if (foto.className == 'img150')
+        arrayFoto.forEach(i => {
+            i.classList.remove('img150')
+        })
 }
