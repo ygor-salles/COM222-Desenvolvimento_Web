2fcd276e4a953c002bfe82b83de5192e0ddfc4ed
unknown <ygorsallesaniceto@gmail.com>
2020-09-17 17:52:42 -0300

    ajustando

--- TRABALHOS/03 - DOM/teste.js
+++
@@ -1,8 +0,0 @@
-function openPresent(event) {
-    const image = event.currentTarget;
-    image.src = 'https://media.giphy.com/media/27ppQUOxe7KlG/giphy.gif';
-    image.removeEventListener('click', openPresent);
-  }
-  
-  const image = document.querySelector('img');
-  image.addEventListener('click', openPresent);
