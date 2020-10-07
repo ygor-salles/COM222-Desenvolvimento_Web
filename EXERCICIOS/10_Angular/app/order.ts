79e2270a394f1a56f125382c53b4c7407380adb5
unknown <ygorsallesaniceto@gmail.com>
2020-10-07 18:46:30 -0300

    Iniciando Angular

---
+++ EXERCICIOS/10_Angular/app/order.ts
@@ -0,0 +1,10 @@
+export class Order {
+   constructor(
+      public name: string,
+      public email: string,
+      public phone: number,
+      public drink: string,
+      public tempPreference: string,
+      public sendText: boolean
+   ){}
+}     
