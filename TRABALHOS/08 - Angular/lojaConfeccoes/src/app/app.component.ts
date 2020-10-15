0b6cbe6c153fbe62d64754cc539e1ca6c10df860
unknown <ygorsallesaniceto@gmail.com>
2020-10-15 16:34:41 -0300

    Alterando Trabalho

--- TRABALHOS/08 - Angular/lojaConfeccoes/src/app/app.component.ts
+++ TRABALHOS/08 - Angular/lojaConfeccoes/src/app/app.component.ts
@@ -11,17 +11,47 @@ export class AppComponent {
   title = 'lojaConfeccoes'
   confirm_msg = ''
 
-  nf: NotaFiscal = {
-    nomeCliente: '',
-    produtosComprados: []
-  }
+  //Capturado do formulário
+  nomeCliente: string
+  produtoComprado: Produto
+  quantidade: number
 
+  //listas de inserção para produtos comprados e suas respectivas quantidades
+  listaProdutosComprados = []
+  listaQuantidade = []
+
+  //Lista de Notas Fiscais
+  listaNF = []
+  tabela = []
+
+  //Lista de produtos pré-definidas que terão que vir do banco
   produtos = [
     new Produto(1, 'Calça Jeans', 10, 0), 
     new Produto(2, 'Blusa Jeans', 50, 0), 
-    new Produto(3, 'Camisa Polo', 10, 0),
+    new Produto(3, 'Camisa Polo', 10, 5.5),
     new Produto(4, 'Bermuda nilon', 5, 1)
   ]
+
+  incluirItem(){
+    console.log(this.nomeCliente)
+    console.log(this.quantidade)
+    console.log(this.produtoComprado.codigo)
+    if (this.quantidade > 0){
+      this.listaProdutosComprados.push(this.produtoComprado)
+      this.listaQuantidade.push(this.quantidade)
+    }
+    this.produtoComprado = null
+    this.quantidade = null
+  }
   
-  
+  fecharNota(){
+    /*
+    for(let i=0; i<=this.listaQuantidade.length; i++){
+      this.tabela = {quantidade: this.listaQuantidade, produto: this.listaQuantidade}
+    }*/
+    this.listaNF.push(new NotaFiscal(this.nomeCliente, this.listaProdutosComprados, this.listaQuantidade))
+    this.listaProdutosComprados = []
+    this.listaQuantidade = []
+  }
+
 }
--- TRABALHOS/08 - Angular/lojaConfeccoes/src/app/nota-fiscal.model.ts
+++ TRABALHOS/08 - Angular/lojaConfeccoes/src/app/nota-fiscal.model.ts
@@ -3,6 +3,7 @@ import { Produto } from './produto.model';
 export class NotaFiscal {
     constructor(
         public nomeCliente: string,
-        public produtosComprados: Array<Produto>
+        public produtosComprados: Array<Produto>,
+        public quantidade: Array<number>
     ) {}
 }
