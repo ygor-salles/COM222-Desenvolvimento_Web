c14129366d3feb1f165524939018fb673e90c73e
unknown <ygorsallesaniceto@gmail.com>
2020-10-16 16:04:12 -0300

    Finalizando Trabalho 08 - Falta o MongoDB

--- TRABALHOS/08 - Angular/lojaConfeccoes/src/app/app.component.ts
+++ TRABALHOS/08 - Angular/lojaConfeccoes/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'lojaConfeccoes'
   confirm_msg = ''
+  nomeClienteTabela = ''
 
   //Capturado do formulário
   nomeCliente: string
@@ -22,7 +23,13 @@ export class AppComponent {
 
   //Lista de Notas Fiscais
   listaNF = []
+
+  //Lista para a tabela que será exibida - Relatório unindo objeto NF + quantidade + totalItem
   tabela = []
+
+  //total
+  totalItem: number
+  totalFinal: number = 0
 
   //Lista de produtos pré-definidas que terão que vir do banco
   produtos = [
@@ -36,11 +43,13 @@ export class AppComponent {
     if (this.quantidade > 0){
       this.listaProdutosComprados.push(this.produtoComprado)
       this.listaQuantidade.push(this.quantidade)
+      this.totalItem = parseFloat(((this.produtoComprado.valorUnitario - (this.produtoComprado.valorUnitario * (this.produtoComprado.desconto/100))) * this.quantidade).toFixed(2))
       this.tabela.push({
         qtd: this.quantidade,
         produto: this.produtoComprado,
-        totalItem: parseFloat(((this.produtoComprado.valorUnitario - (this.produtoComprado.valorUnitario * (this.produtoComprado.desconto/100))) * this.quantidade).toFixed(2))
+        totalItem: this.totalItem
       })
+      this.totalFinal += this.totalItem 
     }
     else {
       alert('Quantidade deve ser maior ou igual a 1')
@@ -53,7 +62,22 @@ export class AppComponent {
     this.listaNF.push(new NotaFiscal(this.nomeCliente, this.listaProdutosComprados))
     this.listaProdutosComprados = []
     this.listaQuantidade = []
+    this.confirm_msg = '$'+this.totalFinal
+    this.nomeClienteTabela = 'Cliente '+this.nomeCliente
     this.nomeCliente = ''
+  }
+
+  novaNota(){
+    this.listaProdutosComprados = []
+    this.listaQuantidade = []
+    this.confirm_msg = ''
+    this.nomeClienteTabela = ''
+    this.nomeCliente = ''
+    this.produtoComprado = null
+    this.quantidade = null
+    this.tabela = []
+    this.totalFinal = 0
+    console.log(this.listaNF)
   }
 
 }
