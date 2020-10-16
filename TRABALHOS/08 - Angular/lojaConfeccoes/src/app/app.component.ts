c3512f83f8975eff84e278f46b3a0533e77ee9ea
unknown <ygorsallesaniceto@gmail.com>
2020-10-16 12:04:34 -0300

    Parte 2

--- TRABALHOS/08 - Angular/lojaConfeccoes/src/app/app.component.ts
+++ TRABALHOS/08 - Angular/lojaConfeccoes/src/app/app.component.ts
@@ -26,32 +26,34 @@ export class AppComponent {
 
   //Lista de produtos pré-definidas que terão que vir do banco
   produtos = [
-    new Produto(1, 'Calça Jeans', 10, 0), 
-    new Produto(2, 'Blusa Jeans', 50, 0), 
-    new Produto(3, 'Camisa Polo', 10, 5.5),
-    new Produto(4, 'Bermuda nilon', 5, 1)
+    new Produto(1234, 'Calça Jeans', 10, 0), 
+    new Produto(2345, 'Blusa Jeans', 50, 0), 
+    new Produto(3456, 'Camisa Polo', 10, 5.5),
+    new Produto(4567, 'Bermuda nilon', 5, 1)
   ]
 
   incluirItem(){
-    console.log(this.nomeCliente)
-    console.log(this.quantidade)
-    console.log(this.produtoComprado.codigo)
     if (this.quantidade > 0){
       this.listaProdutosComprados.push(this.produtoComprado)
       this.listaQuantidade.push(this.quantidade)
+      this.tabela.push({
+        qtd: this.quantidade,
+        produto: this.produtoComprado,
+        totalItem: parseFloat(((this.produtoComprado.valorUnitario - (this.produtoComprado.valorUnitario * (this.produtoComprado.desconto/100))) * this.quantidade).toFixed(2))
+      })
+    }
+    else {
+      alert('Quantidade deve ser maior ou igual a 1')
     }
     this.produtoComprado = null
     this.quantidade = null
   }
   
   fecharNota(){
-    /*
-    for(let i=0; i<=this.listaQuantidade.length; i++){
-      this.tabela = {quantidade: this.listaQuantidade, produto: this.listaQuantidade}
-    }*/
-    this.listaNF.push(new NotaFiscal(this.nomeCliente, this.listaProdutosComprados, this.listaQuantidade))
+    this.listaNF.push(new NotaFiscal(this.nomeCliente, this.listaProdutosComprados))
     this.listaProdutosComprados = []
     this.listaQuantidade = []
+    this.nomeCliente = ''
   }
 
 }
--- TRABALHOS/08 - Angular/lojaConfeccoes/src/app/nota-fiscal.model.ts
+++ TRABALHOS/08 - Angular/lojaConfeccoes/src/app/nota-fiscal.model.ts
@@ -3,7 +3,6 @@ import { Produto } from './produto.model';
 export class NotaFiscal {
     constructor(
         public nomeCliente: string,
-        public produtosComprados: Array<Produto>,
-        public quantidade: Array<number>
+        public produtosComprados: Array<Produto>
     ) {}
 }
